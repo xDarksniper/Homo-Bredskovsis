c24c768c48d472a3ebd7571df27d8eb11ada5351
Bredskov <Nikolaj.bredskov@gmail.com>
2022-08-12 09:46:36 +0200

    stor donut uden tatti sletter det

--- abe.py
+++ abe.py
@@ -110,4 +110,4 @@ while run:
 
 print("Dila stiv  ","Dila ren  ","Dila stor  ","Dila flot  ","Dila skæv, i din mor\n")
 
-print("Brede stor", "brede lil","brede hor","brede simp for lille mie-mor\n")
+print("tatti stor", "brede lil","brede hor","brede simp for lille mie-mor\n")
