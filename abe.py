55000bded1eb4d4f7901f754f6f9ce8538d220d6
Rotten <thorbjoern.j@gmail.com>
2022-08-12 09:39:19 +0200

    Jeg har lavet et godt digt din flotte satan gdfgdfg

--- abe.py
+++ abe.py
@@ -1,1 +1,3 @@
-print("Dila stiv  ","Dila ren  ","Dila stor  ","Dila flot  ","Dila skæv, i din mor")
+print("Dila stiv  ","Dila ren  ","Dila stor  ","Dila flot  ","Dila skæv, i din mor\n")
+
+print("Brede stor", "brede lil","brede hor","brede simp for lille mie-mor\n")
